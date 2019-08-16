92d7a14d45cf5018ba5238a06ecc89b1fe7de9da
sholom <sholomls@gmail.com>
2019-08-16 18:59:05 -0400

    auth services

---
+++ src/app/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+loggedIn = false;
+  constructor() { }
+
+  login() {
+    this.loggedIn = true;
+
+
+    logout() {
+      this.loggedIn = false;
+    }
+}
